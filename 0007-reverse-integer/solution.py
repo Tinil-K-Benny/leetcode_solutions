f2aaa7711b04f660690d357a182968c8e6436fad
Tinil K Benny <145046958+Tinil-K-Benny@users.noreply.github.com>
2026-09-18 06:40:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (33.13%)

---
+++ 0007-reverse-integer/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        is_neg = False
+
+        if x < 0:
+            is_neg = True
+            x = abs(x)
+        res = 0
+        while x > 0:
+            res = (res * 10) + (x % 10)
+            x //= 10
+        
+        if res > 2 ** 31 - 1 or res < -2** 31:
+            return 0
+        
+        if is_neg:
+            return res*-1
+        else:
+            return res        
